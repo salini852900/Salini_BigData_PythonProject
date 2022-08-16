418e5f8adad47015837b07af3cbf385bf1221d82
salini852900 <111185763+salini852900@users.noreply.github.com>
2022-08-16 12:40:06 -0400

    Iteration 3

--- pg-pong.py
+++ pg-pong.py
@@ -5,7 +5,7 @@ import gym
 import datetime
 
 # hyperparameters
-H = 600  # number of hidden layer neurons
+H = 800  # number of hidden layer neurons
 batch_size = 10  # every how many episodes to do a param update?
 learning_rate = 1e-4
 gamma = 0.99  # discount factor for reward
